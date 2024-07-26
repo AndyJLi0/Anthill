d417a8a476b3c73819d98eeedfdda4dadcc608c1
austin <a5gety@gmail.com>
2024-07-25 22:03:56 -0700

    Database added, not working yet

---
+++ shared/FirebaseConfig.mjs
@@ -0,0 +1,40 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { auth, getAuth } from "firebase/auth";
+import { getFirestore, addDoc, collection } from "firebase/firestore";
+// import admin from 'firebase-admin';
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDmxHi0ePlXnK-R9mshM_f6f6uL_9ZB0Lw",
+  authDomain: "anthill-976f5.firebaseapp.com",
+  projectId: "anthill-976f5",
+  storageBucket: "anthill-976f5.appspot.com",
+  messagingSenderId: "404854557178",
+  appId: "1:404854557178:web:ba19d53bbac68de0773c43",
+  measurementId: "G-HX6JM48RCP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const firebaseAuth = getAuth(app);
+
+console.log("before db init");
+// Initialize Firestore
+const db = getFirestore(app);
+
+async function initializeData() {
+  try {
+    await addDoc(collection(db, 'testCollection'), {
+      someField: 'someValue'
+    });
+    console.log('Document successfully written');
+  } catch (e) {
+    console.error('Error adding document: ', e);
+  }
+}
+
+initializeData();
+
+export { firebaseAuth as auth, db};
