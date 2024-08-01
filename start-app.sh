1c2ab619c6206af9649c3ee72d51cdce0d3be4f8
austin <a5gety@gmail.com>
2024-07-31 22:13:23 -0700

    Fixed startup bug with npm install

--- start-app.sh
+++ start-app.sh
@@ -1,5 +1,7 @@
 cd anthill &&
+npm install &&
 npx mocha test --exit --reporter json > public/test-results.txt &&
 cd ../server &&
+npm install &&
 npx mocha test --exit --reporter json > test-results.txt &&
 docker compose up -d
